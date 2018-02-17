4020239fa2a9899b2df329bf01944425ffbec950
Joshua Steinbach <jbst239@g.uky.edu>
2018-02-17 13:05:50 -0500

    some minor upgrades

--- venv/v1.py
+++ venv/v1.py
@@ -34,10 +34,12 @@ def read_in(filename):
 def how_simmilar(a,b):
     sum = 0
     diff = 0
+    count = 0
     at_least_one_in_common = False
     if len(a) > len(b):
         for elem in b.keys():
             if elem in a.keys():
+                count += 1
                 at_least_one_in_common = True
                 diff = a[elem] - b[elem]
                 sum += diff ** 2
@@ -45,28 +47,71 @@ def how_simmilar(a,b):
     else:
         for elem in a.keys():
             if elem in b.keys():
+                count += 1
                 at_least_one_in_common = True
                 diff = a[elem] - b[elem]
                 sum += diff ** 2
 
     if at_least_one_in_common:
         distance = sqrt(sum)
-        return 1/(1+distance)
+        return 1/(1+distance) + count * .001 # this makes it so that for two users
     else:
         return -1
 
 
-def squad(train, k): #finds the nesrest k userd by sommilarity scores
-    for elem in train.keys():
+def how_simmilar_v2(a,b):
+    sum = 0
+    diff = 0
+    count = 0
+    at_least_one_in_common = False
+    if len(a) > len(b):
+        for elem in b.keys():
+            if elem in a.keys():
+                count += 1
+                at_least_one_in_common = True
+                diff = abs(a[elem] - b[elem])
+                sum += (5 - diff)/5
+    else:
+        for elem in a.keys():
+            if elem in b.keys():
+                count += 1
+                at_least_one_in_common = True
+                diff = abs(a[elem] - b[elem])
+                sum += (5 - diff) / 5
 
-    return
+    if at_least_one_in_common:
+        return sum
+    else:
+        return -1
+
+
+def squad(train, user_num, k): #finds the nesrest k userd by sommilarity scores
+    all_sim = []
+    dream_team = []
+    for elem in train.keys():
+        if elem != user_num:
+            friendly_neighborhood_tuple = (how_simmilar_v2(train[user_num], train[elem]), elem)
+            all_sim.append(friendly_neighborhood_tuple)
+    top_k = sorted(all_sim, reverse=True)[0:k]
+    return top_k
+
+
+def unison(u1, u2):
+    uni = []
+    for mov in u1.keys():
+        uni.append(mov)
+    for mov in u2.keys():
+        if mov not in uni:
+            uni.append(mov)
+    return uni
 
 def main():
     starttime = time.time()
     data = read_in("u1-base.base")
     train = organazize(data)
     #pprint(train)
-    print(how_simmilar(train[2],train[3]))
+    #top = squad(train, 1, 2)
+    #pprint(top)
     print("Runtime was: ", time.time() - starttime)
 
 
