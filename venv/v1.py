f06564c0c50773d9cb6b252e8c7162dcdea41b48
Joshua Steinbach <jbst239@g.uky.edu>
2018-02-18 22:58:39 -0500

    lotsa progress

--- venv/v1.py
+++ venv/v1.py
@@ -1,7 +1,7 @@
 from pprint import pprint
 import time
 from math import sqrt
-
+from tqdm import tqdm
 
 def organazize(data): #could possibly implrove time slightly by combining organazize() and read_in()
     train = {}
@@ -59,7 +59,7 @@ def how_simmilar(a,b):
         return -1
 
 
-def how_simmilar_v2(a,b):
+def how_simmilar_v2(a, b):
     sum = 0
     diff = 0
     count = 0
@@ -82,36 +82,99 @@ def how_simmilar_v2(a,b):
     if at_least_one_in_common:
         return sum
     else:
-        return -1
+        return .00001
 
 
-def squad(train, user_num, k): #finds the nesrest k userd by sommilarity scores
+def euclidean(a, b):
+    sum = 0
+    for elem in a.keys():
+        if elem in b.keys():
+            sum += (a[elem]-b[elem])**2
+        else:
+            sum += a[elem]**2
+    for elem in b.keys():
+        if elem not in a.keys():
+            sum += b[elem]**2
+    if sum <= 0:
+        return 0
+    else:
+        return sqrt(sum)
+
+
+def squad(train, user_num, movie_num, k): #finds the nesrest k userd by sommilarity scores
     all_sim = []
     dream_team = []
     for elem in train.keys():
         if elem != user_num:
-            friendly_neighborhood_tuple = (how_simmilar_v2(train[user_num], train[elem]), elem)
-            all_sim.append(friendly_neighborhood_tuple)
-    top_k = sorted(all_sim, reverse=True)[0:k]
+            if movie_num in train[elem].keys():
+                friendly_neighborhood_tuple = (how_simmilar_v2(train[user_num], train[elem]), elem)
+                all_sim.append(friendly_neighborhood_tuple)
+    top_k = sorted(all_sim, reverse=True)[0:k] #need to handle case where there arent k in all_sim
     return top_k
 
 
-def unison(u1, u2):
-    uni = []
-    for mov in u1.keys():
-        uni.append(mov)
-    for mov in u2.keys():
-        if mov not in uni:
-            uni.append(mov)
-    return uni
+def predickt(nearest, train, movie):
+    sum = 0
+    for user in nearest:
+        sum += train[user[1]][movie]
+    if len(nearest) <= 0:
+        return 2.5
+    else:
+        return sum/len(nearest)
+
+def predickt_v2(nearest, train, movie):
+    #pprint(nearest)
+    if len(nearest) <= 0:
+        return 2.5
+    elif len(nearest) == 1:
+        return train[nearest[0][1]][movie]
+    else:
+        sum = 0
+        denom = 0
+        for user in nearest:
+            denom += user[0]
+        for user in nearest:
+            weighted = 1 - (user[0]/denom)
+            sum += (train[user[1]][movie]*weighted)/weighted
+        #print(sum)
+        return sum/10
+
+
+def test_prediction(train):
+    test = read_in("u1-test.test")
+    #pprint(test)
+    sum = 0
+    for elem in tqdm(test):
+        guess = predickt(squad(train, elem[0], elem[1], 10), train, elem[1])
+        actual = elem[2]
+        sum += (guess - actual)**2
+    print("MSE =", sum/len(test))
+
+def test_prediction_k_loop(train, min, max):
+    test = read_in("tryme.txt")
+    # pprint(test)
+    results = []
+    for k in tqdm(range(min, max+1)):
+        sum = 0
+        for elem in test:
+            guess = predickt(squad(train, elem[0], elem[1], k), train, elem[1])
+            actual = elem[2]
+            sum += (guess - actual) ** 2
+        MSE = sum / len(test)
+        tuple = (k,MSE)
+        results.append(tuple)
+    return results
 
 def main():
-    starttime = time.time()
     data = read_in("u1-base.base")
     train = organazize(data)
-    #pprint(train)
-    #top = squad(train, 1, 2)
-    #pprint(top)
+    #print(euclidean(train[2], train[1]))
+    starttime = time.time()
+    pprint(test_prediction_k_loop(train, 5, 10))
+    #movie = 122
+    #top = squad(train, 1, movie, 3)
+    #print(predickt(top,train,movie))
+    #print(top)
     print("Runtime was: ", time.time() - starttime)
 
 
